937d1424fe9c5c38372c057a54791415d437d262
anreh <anurehka@gmail.com>
2023-02-22 11:52:23 +0800

    commit 20230222 First Push -added txtview -Added Button -Added cick event

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.TextViewOnReceiveContentListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Get the Button view from this layout and assign a click
+        //listener is it
+
+        val rollButton:Button=findViewById(R.id.rollBtn)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    /**
+     * Click linster for the Roll Button
+     */
+    private fun rollDice(){
+        Toast.makeText(this, "Dice is Rolled" ,Toast.LENGTH_SHORT).show()
+        val randomInt=(1..6).random()
+
+        val resultText:TextView=findViewById(R.id.txtNumber)
+        resultText.text=randomInt.toString()
     }
 }
